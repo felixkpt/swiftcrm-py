ffe9ac863b25d252176d1a528d71ed4325e12e76
Felix Biwott <felixkpt@gmail.com>
2024-07-07 16:13:46 +0300

    improved model generator

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -17,5 +17,3 @@ from .message import Message
 from .interview import Interview
 from .refresh_token import RefreshToken
 from .token_blacklist import TokenBlacklist
-from .ticket import Ticket
-from .ticket import Ticket
--- app/requests/schemas/ticket.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class TicketSchema(BaseModel):
-    source_id: int
-    comment: str
-
-    class Config:
-        from_attributes = True
