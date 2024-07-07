123dc511246c6697d347d8908b22beb25ac66857
Felix Biwott <felixkpt@gmail.com>
2024-07-07 16:35:09 +0300

    improved schema gen

--- app/requests/schemas/justcat.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class JustcatSchema(BaseModel):
-    name: str = Field(..., max_length=255)
-    description: str = Field(..., max_length=255)
-
-    class Config:
-        from_attributes = True
