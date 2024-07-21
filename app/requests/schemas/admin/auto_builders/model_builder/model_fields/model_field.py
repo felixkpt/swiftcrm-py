65707e49a7b61f2880f152b4a6a9245ce6002c5a
Felix Biwott <felixkpt@gmail.com>
2024-07-21 19:47:31 +0300

    Autobuilder: Edit model_field model and related files

--- app/requests/schemas/admin/auto_builders/model_builder/model_fields/model_field.py
+++ app/requests/schemas/admin/auto_builders/model_builder/model_fields/model_field.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 class ModelFieldSchema(BaseModel):
@@ -6,13 +7,13 @@ class ModelFieldSchema(BaseModel):
     type: str = Field(..., max_length=255)
     label: str = Field(..., max_length=255)
     dataType: str = Field(..., max_length=255)
-    defaultValue: str = Field(..., max_length=255)
+    defaultValue: Optional[str] = Field(None, max_length=255)
     isVisibleInList: bool = Field(..., max_length=None)
     isVisibleInSingleView: bool = Field(..., max_length=None)
     isRequired: bool = Field(..., max_length=None)
     isUnique: bool = Field(..., max_length=None)
-    dropdownSource: str = Field(..., max_length=255)
-    dropdownDependsOn: str = Field(..., max_length=None)
+    dropdownSource: Optional[str] = Field(None, max_length=255)
+    dropdownDependsOn: Optional[str] = Field(None, max_length=None)
     desktopWidth: str = Field(..., max_length=255)
     mobileWidth: str = Field(..., max_length=255)
 
