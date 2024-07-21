09eee54513b5b8b3d9bf78aa40e6e9380efc83a7
Felix Biwott <felixkpt@gmail.com>
2024-07-21 19:58:50 +0300

    Autobuilder: Edit model_builder model and related files

--- app/routes/admin/auto-builders/model-builder/model_builders.py
+++ app/routes/admin/auto-builders/model-builder/model_builders.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from app.repositories.admin.auto_builders.model_builder.model_builder_repo import ModelBuilderRepo as Repo
-from app.requests.schemas.admin.auto_builders.model_builder.model_builder_request import ModelBuilderRequest as ModelSchema
+from app.requests.schemas.admin.auto_builders.model_builder.model_builder import ModelBuilderSchema as ModelSchema
 from app.database.connection import get_db
 from app.events.notifications import NotificationService  # Import NotificationService
 
@@ -12,7 +12,7 @@ repo = Repo()  # Instantiate model repository class
 notification = NotificationService() # Instantiate notification class
 
 # Create a new Model_builder instance.
-@router.post("/", response_model=None)
+@router.post("/", response_model=ModelSchema)
 async def create_route(modelRequest: ModelSchema, db: Session = Depends(get_db)):
     return await repo.create(db=db, model_request=modelRequest)
 
@@ -25,14 +25,13 @@ async def list_route(request: Request, db: Session = Depends(get_db)):
 # Retrieve a single Model_builder by ID.
 @router.get("/{model_id}")
 def view_route(model_id: int, db: Session = Depends(get_db)):
-    print('should fget!')
     result = repo.get(db, model_id=model_id)
     if result is None:
         raise HTTPException(status_code=404, detail=f"Model_builder not found")
     return result
 
 # Update an existing Model_builder by ID.
-@router.put("/{model_id}", response_model=None)
+@router.put("/{model_id}", response_model=ModelSchema)
 async def update_route(model_id: int, modelRequest: ModelSchema, db: Session = Depends(get_db)):
     result = repo.get(db, model_id=model_id)
     if result is None:
