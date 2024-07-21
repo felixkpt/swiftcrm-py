1edad81338fae2f98ecd762cdcd25cc228e5830d
Felix Biwott <felixkpt@gmail.com>
2024-07-21 21:15:56 +0300

    added rships for model builder

--- app/events/notifications.py
+++ app/events/notifications.py
@@ -3,17 +3,19 @@ from app.websocket.websocket_route_handlers import connections
 class NotificationService:
     async def notify_model_updated(self, db, table_name: str, message: str):
         from app.repositories.admin.auto_builders.model_builder.model_builder_repo import ModelBuilderRepo
-        model_id = ModelBuilderRepo.get_page_by_table_name_plural(db, table_name).name_plural
-        # Notify WebSocket clients
-        print(connections)
-        for client_id, connection in connections.items():
-            print(f"Notifying client {client_id} with connection {connection}")
-            try:
-                await connection.send_json({
-                    "client_id": client_id,
-                    "model_id": model_id,
-                    "message": message,
-                })
-                print(f'Notified user: {client_id}')
-            except Exception as e:
-                print(f'NotificationService Error: {e}')
+        res = ModelBuilderRepo.get_page_by_table_name_plural(db, table_name)
+        if res:
+            model_id = res.name_plural
+            # Notify WebSocket clients
+            print(connections)
+            for client_id, connection in connections.items():
+                print(f"Notifying client {client_id} with connection {connection}")
+                try:
+                    await connection.send_json({
+                        "client_id": client_id,
+                        "model_id": model_id,
+                        "message": message,
+                    })
+                    print(f'Notified user: {client_id}')
+                except Exception as e:
+                    print(f'NotificationService Error: {e}')
