7304d14690980af327adf6d402aaca7867b3d57a
Felix Biwott <felixkpt@gmail.com>
2024-07-21 16:31:05 +0300

    added rships for model builder

--- app/models/admin/auto_builders/model_builder/model_builder.py
+++ app/models/admin/auto_builders/model_builder/model_builder.py
@@ -17,9 +17,9 @@ class AdminAutoBuildersModelBuilder(Base):
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
     headers = relationship("AdminAutoBuildersModelBuilderModelHeader",
-                           back_populates="model_builder ", cascade="all, delete-orphan")
+                           back_populates="model_builder", cascade="all, delete-orphan")
     fields = relationship("AdminAutoBuildersModelBuilderModelField",
-                          back_populates="model_builder ", cascade="all, delete-orphan")
+                          back_populates="model_builder", cascade="all, delete-orphan")
     action_labels = relationship("AdminAutoBuildersModelBuilderActionLabel",
-                                 back_populates="model_builder ", cascade="all, delete-orphan")
+                                 back_populates="model_builder", cascade="all, delete-orphan")
  
