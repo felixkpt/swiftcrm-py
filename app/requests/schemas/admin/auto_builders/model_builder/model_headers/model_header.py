92b7cc8456b59f0c5b4f48d31f2229c591933656
Felix Biwott <felixkpt@gmail.com>
2024-07-21 18:57:45 +0300

    Autobuilder: Edit model_header model and related files

--- app/requests/schemas/admin/auto_builders/model_builder/model_headers/model_header.py
+++ app/requests/schemas/admin/auto_builders/model_builder/model_headers/model_header.py
@@ -4,8 +4,8 @@ class ModelHeaderSchema(BaseModel):
     model_builder_id: int = Field(..., max_length=None)
     key: str = Field(..., max_length=255)
     label: str = Field(..., max_length=255)
-    isVisibleInList: str = Field(..., max_length=None)
-    isVisibleInSingleView: str = Field(..., max_length=255)
+    isVisibleInList: bool = Field(..., max_length=None)
+    isVisibleInSingleView: bool = Field(..., max_length=None)
 
     class Config:
         from_attributes = True
