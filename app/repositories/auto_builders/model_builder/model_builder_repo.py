82e8b9635e597121e2c1e1cf31eaa3d9fe0822b7
Felix Biwott <felixkpt@gmail.com>
2024-07-26 00:59:41 +0300

    Autobuilder: Create model_builder model and related files

--- app/repositories/auto_builders/model_builder/model_builder_repo.py
+++ app/repositories/auto_builders/model_builder/model_builder_repo.py
@@ -148,7 +148,7 @@ class ModelBuilderRepo(BaseRepo):
 
     async def update(self, db: Session, model_id: int, model_request):
         generated_data = self.prepare_data(model_request)
-        auto_model_handler(generated_data, db)
+        await auto_model_handler(generated_data, db)
         model_request.name_singular = generated_data['name_singular']
         model_request.name_plural = generated_data['name_plural']
         model_request.class_name = generated_data['class_name']
