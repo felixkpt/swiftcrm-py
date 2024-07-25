367e5aa9a07c1de2ff7ee7542facb1f7c7d33151
Felix Biwott <felixkpt@gmail.com>
2024-07-25 20:59:00 +0300

    Autobuilder: Create model_header model and related files

---
+++ app/models/auto_builders/model_builder/model_headers/model_header_model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, DateTime, ForeignKey, Integer, String, func
+from app.models.base import Base
+from sqlalchemy.orm import relationship
+
+class AutoBuildersModelBuilderModelHeader(Base):
+    __tablename__ = 'auto_builders_model_builder_model_headers'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    model_builder_id = Column(Integer, ForeignKey('auto_builders_model_builder_model_builders.id'))
+    key = Column(String(255))
+    label = Column(String(255))
+    isVisibleInList = Column(Integer)
+    isVisibleInSingleView = Column(Integer)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    status_id = Column(Integer, nullable=False, server_default='1')
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
