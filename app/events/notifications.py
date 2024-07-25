193518e84c9ecf02b9551b3e49c6ab3b5790ca7f
Felix Biwott <felixkpt@gmail.com>
2024-07-26 00:54:41 +0300

    Autobuilder: Create user model and related files

--- app/events/notifications.py
+++ app/events/notifications.py
@@ -2,7 +2,7 @@ from app.websocket.websocket_route_handlers import connections
 
 class NotificationService:
     async def notify_model_updated(self, db, table_name: str, message: str):
-        from app.repositories.admin.auto_builders.model_builder.model_builder_repo import ModelBuilderRepo
+        from app.repositories.auto_builders.model_builder.model_builder_repo import ModelBuilderRepo
         res = ModelBuilderRepo.get_page_by_table_name_plural(db, table_name)
         if res:
             model_id = res.uuid
