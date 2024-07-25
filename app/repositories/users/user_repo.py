c4e3f29defc705cc5d9ced02ef285b9f96af8fff
Felix Biwott <felixkpt@gmail.com>
2024-07-26 02:15:09 +0300

    Autobuilder: Create user model and related files

--- app/repositories/users/user_repo.py
+++ app/repositories/users/user_repo.py
@@ -18,7 +18,7 @@ class UserRepo(BaseRepo):
 
     async def list(self, db: Session, request: Request):
         query_params = get_query_params(request)
-        search_fields = ['first_name', 'email', 'phone_number', 'password', 'password_confirmation']
+        search_fields = ['first_name', 'email', 'password', 'password_confirmation']
 
         query = db.query(Model)
         query = apply_common_filters(query, Model, search_fields, query_params)
@@ -52,6 +52,9 @@ class UserRepo(BaseRepo):
         value = query_params.get('phone_number', '').strip()
         if isinstance(value, str) and len(value) > 0:
             query = query.filter(Model.phone_number.ilike(f'%{value}%'))
+        value = query_params.get('alternate_phone', '').strip()
+        if isinstance(value, str) and len(value) > 0:
+            query = query.filter(Model.alternate_phone.ilike(f'%{value}%'))
         value = query_params.get('password', '').strip()
         if isinstance(value, str) and len(value) > 0:
             query = query.filter(Model.password.ilike(f'%{value}%'))
@@ -65,7 +68,7 @@ class UserRepo(BaseRepo):
         return query
 
     async def create(self, db: Session, model_request):
-        required_fields = ['first_name', 'email', 'phone_number', 'password', 'password_confirmation']
+        required_fields = ['first_name', 'email', 'password', 'password_confirmation']
         unique_fields = ['email']
         Validator.validate_required_fields(model_request, required_fields)
         UniqueChecker.check_unique_fields(db, Model, model_request, unique_fields)
@@ -76,6 +79,7 @@ class UserRepo(BaseRepo):
             last_name = str(model_request.last_name).strip(),
             email = str(model_request.email).strip(),
             phone_number = str(model_request.phone_number).strip(),
+            alternate_phone = str(model_request.alternate_phone).strip(),
             password = model_request.password,
             password_confirmation = model_request.password_confirmation,
             user_id = current_user_id,
@@ -93,7 +97,7 @@ class UserRepo(BaseRepo):
         return db_query
 
     async def update(self, db: Session, model_id: int, model_request):
-        required_fields = ['first_name', 'email', 'phone_number', 'password', 'password_confirmation']
+        required_fields = ['first_name', 'email', 'password', 'password_confirmation']
         unique_fields = ['email']
         Validator.validate_required_fields(model_request, required_fields)
         UniqueChecker.check_unique_fields(db, Model, model_request, unique_fields, model_id)
@@ -105,6 +109,7 @@ class UserRepo(BaseRepo):
             db_query.last_name = str(model_request.last_name).strip()
             db_query.email = str(model_request.email).strip()
             db_query.phone_number = str(model_request.phone_number).strip()
+            db_query.alternate_phone = str(model_request.alternate_phone).strip()
             db_query.password = model_request.password
             db_query.password_confirmation = model_request.password_confirmation
             db_query.user_id = current_user_id
