af4f9f5e26f2de072501b5eb3b02784da1708499
Felix Biwott <felixkpt@gmail.com>
2024-07-26 02:29:18 +0300

    Autobuilder: Create model_field model and related files

--- app/requests/schemas/auto_builders/model_builder/model_builder_request.py
+++ app/requests/schemas/auto_builders/model_builder/model_builder_request.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 from app.requests.schemas.auto_builders.model_builder.model_builder_schema import ModelBuilderSchema
 from app.requests.schemas.auto_builders.model_builder.model_headers.model_header_schema import ModelHeaderSchema
 from app.requests.schemas.auto_builders.model_builder.model_fields.model_field_schema import ModelFieldSchema
-from app.requests.schemas.auto_builders.model_builder.action_labels.action_label import ActionLabelSchema
+from app.requests.schemas.auto_builders.model_builder.action_labels.action_label_schema import ActionLabelSchema
 
 class ModelHeaderSchemaWithoutID(ModelHeaderSchema):
     model_builder_id: Optional[int] = None
