54bdfab17908ef324148d1fc7abd3f97913a3c94
Felix Biwott <felixkpt@gmail.com>
2024-07-26 02:22:35 +0300

    Autobuilder: Create user model and related files

--- app/requests/schemas/auto_builders/model_builder/model_builder_request.py
+++ app/requests/schemas/auto_builders/model_builder/model_builder_request.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
-from app.requests.schemas.auto_builders.model_builder.model_builder import ModelBuilderSchema
-from app.requests.schemas.auto_builders.model_builder.model_headers.model_header import ModelHeaderSchema
-from app.requests.schemas.auto_builders.model_builder.model_fields.model_field import ModelFieldSchema
+from app.requests.schemas.auto_builders.model_builder.model_builder_schema import ModelBuilderSchema
+from app.requests.schemas.auto_builders.model_builder.model_headers.model_header_schema import ModelHeaderSchema
+from app.requests.schemas.auto_builders.model_builder.model_fields.model_field_schema import ModelFieldSchema
 from app.requests.schemas.auto_builders.model_builder.action_labels.action_label import ActionLabelSchema
 
 class ModelHeaderSchemaWithoutID(ModelHeaderSchema):
