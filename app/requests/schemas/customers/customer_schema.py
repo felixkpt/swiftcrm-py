804862d14b72cee73d9aa4ec4fea8e959f330dea
Felix Biwott <felixkpt@gmail.com>
2024-08-28 09:18:58 +0300

    Autobuilder: Create customer model and related files

---
+++ app/requests/schemas/customers/customer_schema.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class CustomerSchema(BaseModel):
+    name: str = Field(..., max_length=255)
+    last_name: str = Field(..., max_length=255)
+    phone_number: str = Field(..., max_length=255)
+    email: str = Field(..., max_length=255)
+    alternate_number: str = Field(..., max_length=255)
+    user_id: Optional[int] = Field(None, max_length=None)
+
+    class Config:
+        from_attributes = True
+        protected_namespaces = ()
