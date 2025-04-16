eade6b9ceabb279be5f1c9cc2c509415bb35a94c
Felix Kiptoo <felixkpt@gmail.com>
2025-04-16 23:08:08 +0300

    Autobuilder: Create chatbot model and related files

---
+++ app/models/api/chatbots/chatbot_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey, DateTime, Integer, String, func
+from app.models.base import Base
+from sqlalchemy.orm import relationship
+
+class ApiChatbot(Base):
+    __tablename__ = '_api_chatbots'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), unique=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    status_id = Column(Integer, nullable=False, server_default='1')
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
---
+++ app/requests/schemas/api/chatbots/chatbot_schema.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class ChatbotSchema(BaseModel):
+    name: str = Field(..., max_length=255)
+    user_id: Optional[int] = Field(None, max_length=None)
+
+    class Config:
+        from_attributes = True
+        protected_namespaces = ()
